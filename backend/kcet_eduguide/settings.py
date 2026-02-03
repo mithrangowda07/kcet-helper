e7e834e5051d853c95bc1d8d19f5d582d427ff4a
mithrangowda07 <mithrangowda01@gmail.com>
2026-02-03 23:14:17 +0530

    Improvements

--- backend/kcet_eduguide/settings.py
+++ backend/kcet_eduguide/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'kcet_eduguide.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbms_project',
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME',''),
+        'USER': os.getenv('DB_USER',''),
+        'PASSWORD': os.getenv('DB_PASSWORD',''),
+        'HOST': os.getenv('DB_HOST',''),
+        'PORT': os.getenv('DB_PORT',''),
         'OPTIONS': {
             'ssl_mode': 'REQUIRED'
-        },
+        }
     }
 }
 
